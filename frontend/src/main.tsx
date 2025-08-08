2d2d3d7599db6477be927c3205df1d095f295956
João André Pinheiro Canais <uc2023213000@student.uc.pt>
2025-08-08 12:51:34 +0100

    tentatva 291

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
-
-// Handle GitHub Pages routing
-if (window.location.search.includes('/?/')) {
-  const url = window.location.search.split('/?/')[1]
-  if (url) {
-    window.history.replaceState(null, '', '/' + url.replace(/~/g, '&'))
-  }
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )

380cff0e385b4364f5fab2d1e383c2156757a97d
João André Pinheiro Canais <uc2023213000@student.uc.pt>
2025-08-14 05:07:22 +0100

    StatisticsByDistrict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage'
 import RegisterPage from './pages/RegisterPage'
 
 function App() {
-
   return (
     <Routes> 
       <Route path="/" element={<LandingPage/>} />
       <Route path="/register" element={<RegisterPage/>} />
-      <Route path="*" element={<LandingPage/>} />
+      {/* Redireciona todas as outras rotas para a página principal */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
